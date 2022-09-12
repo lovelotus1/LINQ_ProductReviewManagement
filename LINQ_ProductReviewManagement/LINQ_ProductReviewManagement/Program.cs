1dc40993e6b72f8221f1eec502d59c577158f63e
“Love <opera.prem@gmail.com>
2022-09-12 20:56:48 +0530

    Added Welcome Message

---
+++ LINQ_ProductReviewManagement/LINQ_ProductReviewManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQ_ProductReviewManagement
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to product review Management");
+        }
+    }
+}
