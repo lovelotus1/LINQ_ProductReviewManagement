de8c2901200241333ed2e58fa0123643b207ddc2
“Love <opera.prem@gmail.com>
2022-09-12 22:35:42 +0530

    Added: Create Data Table

--- LINQ_ProductReviewManagement/LINQ_ProductReviewManagement/Operations.cs
+++ LINQ_ProductReviewManagement/LINQ_ProductReviewManagement/Operations.cs
@@ -64,5 +64,45 @@ namespace LINQ_ProductReviewManagement
                 Console.WriteLine("product Id:-" + list.ProductId + " Review :-" + list.Review);
             }
         }
+        public void CreateDataTable()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("ProductID");
+            table.Columns.Add("UserID");
+            table.Columns.Add("Review");
+            table.Columns.Add("Rating");
+            table.Columns.Add("IsLike");
+
+            table.Rows.Add("1", "1", "Best", "5", "True");
+            table.Rows.Add("2", "1", "Good", "4", "True");
+            table.Rows.Add("3", "1", "Average", "3", "True");
+            table.Rows.Add("4", "2", "Bad", "2", "False");
+            table.Rows.Add("5", "2", "Best", "5", "True");
+            table.Rows.Add("6", "2", "Good", "4", "True");
+            table.Rows.Add("7", "3", "Average", "3", "True");
+            table.Rows.Add("8", "3", "Bad", "2", "False");
+            table.Rows.Add("9", "3", "Best", "5", "True");
+            table.Rows.Add("10", "4", "Good", "4", "True");
+            table.Rows.Add("11", "4", "Average", "3", "True");
+            table.Rows.Add("12", "4", "Bad", "2", "False");
+            table.Rows.Add("13", "5", "Best", "5", "True");
+            table.Rows.Add("14", "5", "Good", "4", "True");
+            table.Rows.Add("15", "5", "Average", "3", "True");
+            table.Rows.Add("16", "6", "Bad", "2", "False");
+            table.Rows.Add("17", "6", "Best", "5", "True");
+            table.Rows.Add("18", "6", "Good", "4", "True");
+            table.Rows.Add("19", "7", "Average", "3", "True");
+            table.Rows.Add("20", "7", "Bad", "2", "False");
+            table.Rows.Add("21", "7", "Best", "5", "True");
+            table.Rows.Add("22", "8", "Good", "4", "True");
+            table.Rows.Add("23", "8", "Average", "3", "True");
+            table.Rows.Add("24", "9", "Bad", "2", "False");
+            table.Rows.Add("25", "9", "Best", "5", "True");
+            foreach (DataRow data in table.Rows)
+            {
+                Console.WriteLine("==================================================================================");
+                Console.WriteLine("ProductID: {0}\t UserID: {1}\t Review: {2}\t Rating: {3}\t IsLike: {4}\t", data[0], data[1], data[2], data[3], data[4]);
+            }
+        }
     }
 }
